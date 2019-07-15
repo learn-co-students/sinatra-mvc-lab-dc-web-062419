672fd45983a10d1cb73543b92724b87cd8137422
Luke O'Malley <Lukeomalley95@gmail.com>
2019-07-15 15:59:35 -0400

    Completed Lab 🤙

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pig_latinizer = PigLatinizer.new
+    @latin = pig_latinizer.piglatinize(params[:user_phrase])
+    erb :pig_latin
+  end
 end
